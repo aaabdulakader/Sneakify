baa066b0819e26ebd7762a70977338df4cf9e475
Abdulkader A Abdi <69095293+aaabdulakader@users.noreply.github.com>
2024-03-10 15:13:44 -0500

    initial Commit

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 const router = express.Router();
