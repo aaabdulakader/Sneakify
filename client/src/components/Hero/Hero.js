4d8986361be78482e74ed9068768283f6ed1bbaa
Abdulkader A Abdi <69095293+aaabdulakader@users.noreply.github.com>
2024-05-09 18:32:21 -0500

    minor fixes

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,15 +1,23 @@
-import React from 'react'; 
+import { React, useEffect, useState } from "react";
 
- import heroStyles from './Hero.module.css';
-
- 
+import heroStyles from "./Hero.module.css";
 
 function Hero() {
-    return (
-        <div>
-            {/* Hero component */}
-        </div>
-    );
+  const [topProducts, setTopProducts] = useState([]);
+  // get top products for hero slideshow
+  const link = "/products/top";
+
+  useEffect(() => {
+    function fetchData() {
+      fetch(link)
+        .then((response) => response.json())
+        .then((data) => setTopProducts(data.documents));
+    }
+
+    fetchData();
+  }, [link]);
+
+  return <div></div>;
 }
 
 export default Hero;
